a37cc3ec865d012c57dd71835e113383d62fa426
shinancao <shinancao666@163.com>
2018-11-06 20:22:19 +0800

    add parse category

--- grapKeywords.py
+++ grapKeywords.py
@@ -4,19 +4,46 @@
 import urllib.request
 import re
 
+from urllib.parse import urlparse
+import os.path
 
-def getAllProductsUrl(companyUrl):
-    respData = getResponseData(companyUrl)
-    allPaths = re.findall(r'</span>\\n\\t\\t\\t\\t<a href="/product/(\d+)-(\d+)/(.*?)\.html', str(respData))
 
-    scheme = 'https://www.alibaba.com/product-detail/'
+def getCategorysUrl(companyUrl):
+    allCategoryUrl = companyUrl + "/productlist.html"
+    respData = getResponseData(allCategoryUrl)
+    listUrl = re.findall(r'<ul class="productgroup-list">')
+
+
+def getAllProductsUrl(productListUrl):
+    parsedUri = urlparse(productListUrl)
+    schemeAndProtocol = "{uri.scheme}://{uri.netloc}/".format(uri=parsedUri)
+    paths = "{uri.path}".format(uri=parsedUri).split("/")
+    temp = paths[1].split("-") #productgrouplist-803523792-1
+    index = 1
+    flag = True
 
     productsUrl = []
-    for path in allPaths:
-        url = scheme + path[2] + "_" + path[0] + ".html"
-        productsUrl.append(url)
+
+    while flag:
+        pageUrl = schemeAndProtocol + temp[0] + "-" + temp[1] + "-" + str(index) + "/" + paths[2]
+        respData = getResponseData(pageUrl)
+        if "No matching results." not in str(respData):
+
+            allPaths = re.findall(r'</span>\\n\\t\\t\\t\\t<a href="/product/(\d+)-(\d+)/(.*?)\.html', str(respData))
+
+            scheme = 'https://www.alibaba.com/product-detail/'
+
+            for path in allPaths:
+                url = scheme + path[2] + "_" + path[0] + ".html"
+                productsUrl.append(url)
+        else:
+            flag = False
+
+        index += 1
 
     return productsUrl
+
+
 
 def getKeywordsAndTitle(productUrl):
     respData = getResponseData(productUrl)
@@ -39,13 +66,11 @@ def getResponseData(htmlUrl):
     respData = resp.read()
     return respData
 
-from urllib.parse import urlparse
-from os.path import normpath, basename
 import time
 def getFileName(companyUrl):
     parsedUri = urlparse(companyUrl)
-    host = '{uri.netloc}'.format(uri=parsedUri)
-    lastPart = basename(normpath(companyUrl)).replace(".html", "")
+    host = "{uri.netloc}".format(uri=parsedUri)
+    lastPart = os.path.basename(os.path.normpath(companyUrl)).replace(".html", "")
     nowTime = int(time.time())
     return host + "_" + lastPart + "_" + str(nowTime) + ".txt"
 
@@ -53,14 +78,16 @@ def getFileName(companyUrl):
 """开始处理"""
 print("Enter the company url:")
 
-companyUrl = input().strip()
+#companyUrl = input().strip()
 
 print("Doing ......")
 
 results = ""
 
-productsUrl = getAllProductsUrl(companyUrl)
+productsUrl = getAllProductsUrl("https://younalchina.en.alibaba.com/productgrouplist-804089749/WPC_Wall_Panel.html")
+print(productsUrl)
 
+"""
 for url in productsUrl[0:4]:
     dict = getKeywordsAndTitle(url)
     if not (dict is None):
@@ -78,8 +105,8 @@ f.close()
 
 import os
 
-print("Done! Saved in file:")
-print(os.path.realpath(filename))
+print("Done!")
+print("Saved in file: " + os.path.realpath(filename)) """
 
 
 
